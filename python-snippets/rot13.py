209e311e0e2bcc902dd06f0cd80e1dc2ffd66780
Ivan Alejandro <ivanalejandro0@gmail.com>
2013-09-13 14:25:27 -0300

    Fix rot13 function and improve output.

--- python-snippets/rot13.py
+++ python-snippets/rot13.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import string
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 alphabet_plus13 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
-rot13_trans = str.maketrans(alphabet, alphabet_plus13)
+rot13_trans = string.maketrans(alphabet, alphabet_plus13)
 
 
 def rot13(text):
     return text.translate(rot13_trans)
 
 if __name__ == '__main__':
-    rot13('Hello world!')
+    msg = 'Hello world!'
+    msg_rot13 = rot13(msg)
+    msg2 = rot13(msg_rot13)
+
+    print "Original message: {0}".format(msg)
+    print "Original message through rot13: {0}".format(msg_rot13)
+    print "Original message through rot13 twice: {0}".format(msg2)
